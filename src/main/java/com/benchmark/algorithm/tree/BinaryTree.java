eb3d89c437b5ab5212499821a474a2e5fcccaa84
zhangyebai <zhangyebai@126.com>
2018-05-17 17:27:01 +0800

    add un-completed binary tree method

--- src/main/java/com/benchmark/algorithm/tree/BinaryTree.java
+++ src/main/java/com/benchmark/algorithm/tree/BinaryTree.java
@@ -45,7 +45,7 @@ public class BinaryTree {
 					break;
 				}
 			}
-			/// turn left
+			/// turn right
 			else if (parent.getData() < value){
 				index = parent.getRight();
 				if(null == index){
@@ -148,4 +148,65 @@ public class BinaryTree {
 		}
 		return result;
 	}
+
+	/**
+	 * @apiNote 二叉树的删除操作有一点需要注意的事情: 查找需要删除的节点的时候,
+	 * 			需要查找该节点的父节点,以及要删除的目标节点是父节点的左子树还是右子树,
+	 * 			简而言之就是需要目标节点及其父节点的信息
+	 *
+	 * step 1 : 查找要删除的节点以及父节点
+	 * step 2 : 要删除的是叶节点
+	 * step 3 : 要删除的是单叉子节点
+	 * step 4 : 要删除的是二叉子节点
+	 * */
+	public boolean delete(int value){
+		boolean result = false;
+		if(null == this.root){
+			return result;
+		}
+		Node index = this.root;
+		Node parrent = null;
+		boolean isLeftNode = false;
+
+		for(; index != null;){
+
+			//turn left
+			 if(index.getData() > value){
+				parrent = index;
+				index = index.getLeft();
+				 isLeftNode = true;
+			}
+			//turn right
+			else if(index.getData() < value){
+			 	parrent = index;
+			 	index = index.getRight();
+				 isLeftNode = false;
+			 }
+			 else{
+				break;
+			 }
+		}
+
+		if(null == index){
+			return false;
+		}
+
+		if(null == index.getLeft() && null == index.getRight()){
+			if(index == this.root){
+				this.root = null;
+				return true;
+			}
+		}
+
+		return result;
+	}
+
+
+	/**
+	 * orphan 孤儿
+	 * */
+	public Node voteRoot(Node orphan){
+		return null;
+	}
+
 }
